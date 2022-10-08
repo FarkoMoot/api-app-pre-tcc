27c40bb558f305e8297c4331c357e1264139fde0
Farko Moot <farko_moot@hotmail.com>
2022-10-08 12:39:34 -0300

    agr vai

--- src/index.js
+++ src/index.js
@@ -144,11 +144,10 @@ app.get('/teste', async (req, res)=>{
 app.get('/', (req, res)=>{
   res.send("Hello World!");
 })
-const DB_USER = 'FarkoAdm'
-const DB_PASSWORD = 'jXhS5X8Apffw9wHk'
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@clusterfutebol.xfuowcs.mongodb.net/?retryWrites=true&w=majority`)
+
+mongoose.connect(`mongodb+srv://${db_user}:${db_pass}@clusterfutebol.xfuowcs.mongodb.net/?retryWrites=true&w=majority`)
 .then(()=>{
-    app.listen(3000);
+    app.listen(port);
     console.log("Conectamos!!!");
   }).catch((error)=>{
     console.log(error);
